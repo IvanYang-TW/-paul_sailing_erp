7e7bc9bbed8d30997524ed3e76521e0d971a30f5
IvanYang <lovable_9899@hotmail.com>
2023-07-23 22:52:58 +0800

    dbf File Convert to Excel

---
+++ FileConvertTool/FileConvertTool/FileService.cs
@@ -0,0 +1,270 @@
+﻿using DbfDataReader;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace FileConvertTool
+{
+    public class FileService
+    {
+        /// <summary>
+        /// dbf檔轉換成csv檔
+        /// </summary>
+        /// <param name="dbfPath"></param>
+        public static void DbfConvertToCsv(string dbfPath)
+        {
+            try
+            {
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                using (var dbfTable = new DbfTable(dbfPath, Encoding.GetEncoding("big5")))
+                {
+                    var header = dbfTable.Header;
+                    var versionDescription = header.VersionDescription;
+                    var hasMemo = dbfTable.Memo != null;
+                    var recordCount = header.RecordCount;
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendLine(string.Join(",", dbfTable.Columns.Select(x => x.ColumnName)));
+                    // and to iterate over the rows
+                    var skipDeleted = true;
+                    var dbfRecord = new DbfRecord(dbfTable);
+
+
+                    while (dbfTable.Read(dbfRecord))
+                    {
+                        if (skipDeleted && dbfRecord.IsDeleted)
+                        {
+                            continue;
+                        }
+
+                        sb.AppendLine(string.Join(",", dbfRecord.Values.Select(x => x.ToString())));
+                    }
+                    File.WriteAllText(Path.ChangeExtension(dbfPath, "csv"), sb.ToString(), Encoding.GetEncoding("big5"));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+        /// <summary>
+        /// dbf檔轉換成DataTable
+        /// </summary>
+        /// <param name="dbfPath"></param>
+        /// <returns></returns>
+        public static DataTable DbfConvertToDataTable(string dbfPath)
+        {
+            try
+            {
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                using (var dbfTable = new DbfTable(dbfPath, Encoding.GetEncoding("big5")))
+                {
+                    var header = dbfTable.Header;
+                    var versionDescription = header.VersionDescription;
+                    var hasMemo = dbfTable.Memo != null;
+                    var recordCount = header.RecordCount;
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendLine(string.Join(",", dbfTable.Columns.Select(x => x.ColumnName)));
+                    DataTable dt = new DataTable();
+                    DataRow row = null;
+                    dt.Columns.AddRange(dbfTable.Columns
+                        .Select(x=>new DataColumn { 
+                            ColumnName = x.ColumnName,
+                            DataType = x.DataType,
+                        }).ToArray());
+                    // and to iterate over the rows
+                    var skipDeleted = true;
+                    var dbfRecord = new DbfRecord(dbfTable);
+
+                    while (dbfTable.Read(dbfRecord))
+                    {
+                        if (skipDeleted && dbfRecord.IsDeleted)
+                        {
+                            continue;
+                        }
+                        row = dt.NewRow();
+                        for (int i = 0; dbfRecord.Values.Count > 0; i++)
+                        {
+                            row[i] = dbfRecord.Values[i];
+                        }
+                        dt.Rows.Add(row);
+                    }
+                    return dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+        /// <summary>
+        /// dbf檔篩選
+        /// </summary>
+        /// <param name="dbfPath"></param>
+        /// <returns></returns>
+        public static DataTable DbfFilterData(string dbfPath)
+        {
+            try
+            {
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                using (var dbfTable = new DbfTable(dbfPath, Encoding.GetEncoding("big5")))
+                {
+                    var header = dbfTable.Header;
+                    var versionDescription = header.VersionDescription;
+                    var hasMemo = dbfTable.Memo != null;
+                    var recordCount = header.RecordCount;
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendLine(string.Join(",", dbfTable.Columns.Select(x => x.ColumnName)));
+                    DataTable dt = new DataTable();
+                    DataRow row = null;
+                    dt.Columns.AddRange(dbfTable.Columns
+                        .Select(x=>new DataColumn { 
+                            ColumnName = x.ColumnName,
+                            DataType = x.DataType,
+                        }).ToArray());
+                    // and to iterate over the rows
+                    var skipDeleted = true;
+                    var dbfRecord = new DbfRecord(dbfTable);
+
+                    while (dbfTable.Read(dbfRecord))
+                    {
+                        if (skipDeleted && dbfRecord.IsDeleted)
+                        {
+                            continue;
+                        }
+                        row = dt.NewRow();
+                        for (int i = 0; dbfRecord.Values.Count > 0; i++)
+                        {
+                            row[i] = dbfRecord.Values[i];
+                        }
+                        dt.Rows.Add(row);
+                    }
+                    return dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+        /// <summary>
+        /// 儲存DataTable為Excel檔
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="xlsxFilename"></param>
+        public static void SaveDataTableAsExcel(DataTable dt, string xlsxFilename)
+        {
+            ////建立Excel 2007檔案
+            IWorkbook wb = new XSSFWorkbook();
+            ISheet ws;
+
+            if (dt.TableName != string.Empty)
+            {
+                ws = wb.CreateSheet(dt.TableName);
+            }
+            else
+            {
+                ws = wb.CreateSheet("Sheet1");
+            }
+
+            ws.CreateRow(0);//第一行為欄位名稱
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                ws.GetRow(0).CreateCell(i).SetCellValue(dt.Columns[i].ColumnName);
+            }
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                ws.CreateRow(i + 1);
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    ws.GetRow(i + 1).CreateCell(j).SetCellValue(dt.Rows[i][j].ToString());
+                }
+            }
+
+            FileStream file = new FileStream(xlsxFilename, FileMode.Create);//產生檔案
+            wb.Write(file);
+            file.Close();
+        }
+        /// <summary>
+        /// dbf檔案轉換成Excel檔
+        /// </summary>
+        /// <param name="sheetName"></param>
+        /// <param name="dbfPath"></param>
+        public static void DbfConvertToExcel(string sheetName, string dbfPath)
+        {
+            try
+            {
+                ////建立Excel 2007檔案
+                IWorkbook wb = new XSSFWorkbook();
+                ISheet ws;
+
+                if (sheetName != string.Empty)
+                {
+                    ws = wb.CreateSheet(sheetName);
+                }
+                else
+                {
+                    ws = wb.CreateSheet("Sheet1");
+                }
+
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                using (var dbfTable = new DbfTable(dbfPath, Encoding.GetEncoding("big5")))
+                {
+                    var header = dbfTable.Header;
+                    var versionDescription = header.VersionDescription;
+                    var hasMemo = dbfTable.Memo != null;
+                    var recordCount = header.RecordCount;
+
+                    ws.CreateRow(0);//第一行為欄位名稱
+                    for (int i = 0; i < dbfTable.Columns.Count; i++)
+                    {
+                        ws.GetRow(0).CreateCell(i).SetCellValue(dbfTable.Columns[i].ColumnName);
+                    }
+
+                    // and to iterate over the rows
+                    var skipDeleted = true;
+                    var dbfRecord = new DbfRecord(dbfTable);
+
+                    int iRow = 0;
+                    while (dbfTable.Read(dbfRecord))
+                    {
+                        if (skipDeleted && dbfRecord.IsDeleted)
+                        {
+                            continue;
+                        }
+
+                        ws.CreateRow(iRow + 1);
+                        for (int j = 0; j < dbfTable.Columns.Count; j++)
+                        {
+                            ws.GetRow(iRow + 1).CreateCell(j).SetCellValue(dbfRecord.Values[j].ToString());
+                        }
+                        iRow++;
+                    }
+                    FileStream file = new FileStream(Path.ChangeExtension(dbfPath, "xlsx"), FileMode.Create);//產生檔案
+                    wb.Write(file);
+                    wb.Dispose();
+                    file.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ FileConvertTool/FileConvertTool/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace FileConvertTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn_dbfFileConvert = new System.Windows.Forms.Button();
+            folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            richTextBox1 = new System.Windows.Forms.RichTextBox();
+            btn_directoryDbfConvertToExcel = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // btn_dbfFileConvert
+            // 
+            btn_dbfFileConvert.Location = new System.Drawing.Point(30, 12);
+            btn_dbfFileConvert.Name = "btn_dbfFileConvert";
+            btn_dbfFileConvert.Size = new System.Drawing.Size(195, 23);
+            btn_dbfFileConvert.TabIndex = 0;
+            btn_dbfFileConvert.Text = "單檔案_執行dbf轉檔Excel";
+            btn_dbfFileConvert.UseVisualStyleBackColor = true;
+            btn_dbfFileConvert.Click += btn_dbfFileConvert_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new System.Drawing.Point(21, 93);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new System.Drawing.Size(410, 267);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            // 
+            // btn_directoryDbfConvertToExcel
+            // 
+            btn_directoryDbfConvertToExcel.Location = new System.Drawing.Point(30, 54);
+            btn_directoryDbfConvertToExcel.Name = "btn_directoryDbfConvertToExcel";
+            btn_directoryDbfConvertToExcel.Size = new System.Drawing.Size(195, 23);
+            btn_directoryDbfConvertToExcel.TabIndex = 2;
+            btn_directoryDbfConvertToExcel.Text = "整批轉換_執行dbf轉檔Excel";
+            btn_directoryDbfConvertToExcel.UseVisualStyleBackColor = true;
+            btn_directoryDbfConvertToExcel.Click += btn_directoryDbfConvertToExcel_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(451, 375);
+            Controls.Add(btn_directoryDbfConvertToExcel);
+            Controls.Add(richTextBox1);
+            Controls.Add(btn_dbfFileConvert);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_dbfFileConvert;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn_directoryDbfConvertToExcel;
+    }
+}
+
